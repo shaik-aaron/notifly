2cf66df7991b2ce5dd756b628d2fb54de5549fd8
shaik-aaron <shaikaaronbbhs@gmail.com>
2023-02-28 16:26:04 +0530

    Fixed Multiple Select Bug

--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,19 @@
-import auth from '../firebase-auth';
 import database from '../firebase-database';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {faRightFromBracket} from '@fortawesome/free-solid-svg-icons';
-import {faMugSaucer} from '@fortawesome/free-solid-svg-icons';
-import {faDumbbell} from '@fortawesome/free-solid-svg-icons';
-import {faBurger} from '@fortawesome/free-solid-svg-icons';
-import {faHouse} from '@fortawesome/free-solid-svg-icons';
-import {set, ref, onValue} from 'firebase/database';
+import {
+  faRightFromBracket,
+  faMugSaucer,
+  faDumbbell,
+  faBurger,
+  faHouse,
+} from '@fortawesome/free-solid-svg-icons';
+import {set, ref} from 'firebase/database';
 import {createAvatar} from '@dicebear/core';
 import {SvgXml} from 'react-native-svg';
 import {adventurer} from '@dicebear/collection';
+import {useNavigation} from '@react-navigation/native';
+import {getAuth, signOut} from 'firebase/auth';
 import {
   Text,
   View,
@@ -17,13 +21,15 @@ import {
   Alert,
   TextInput,
   Pressable,
-  KeyboardAvoidingView,
 } from 'react-native';
 import React, {useState} from 'react';
 
 import styles from './Stylesheets/Home-Stylesheet';
 
 const Home = () => {
+  const auth = getAuth();
+  const navigation = useNavigation();
+
   //This creates the avatar
   const avatar = createAvatar(adventurer, {
     seed: auth.currentUser?.displayName ?? 'Default',
@@ -42,10 +48,6 @@ const Home = () => {
   const svg = avatar.toString();
   const [time, setTime] = useState('');
   const [location, setLocation] = useState('');
-  const [isGym, setIsGym] = useState(false);
-  const [isSendhoor, setIsSendhoor] = useState(false);
-  const [isIfun, setIsIfun] = useState(false);
-  const [isSoumil, setIsSoumil] = useState(false);
   const [isSent, setIsSent] = useState(false);
 
   //In case the user hits the back button (Temp fix)
@@ -66,105 +68,136 @@ const Home = () => {
     backAction,
   );
 
+  function handleSignOut() {
+    signOut(auth)
+      .then(() => {
+        navigation.navigate('Login');
+      })
+      .catch(error => alert(error.message));
+  }
+
   function setGym() {
     setLocation('Gym');
-    setIsGym(prev => !prev);
   }
 
   function setSendhoor() {
     setLocation('Sendhoor');
-    setIsSendhoor(prev => !prev);
   }
 
   function setIfun() {
     setLocation('ifun');
-    setIsIfun(prev => !prev);
   }
 
   function setSoumil() {
     setLocation(`Soumil's house`);
-    setIsSoumil(prev => !prev);
   }
 
   //Main function
   return (
     <View style={styles.container}>
-      <View style={styles.navbar}>
-        <SvgXml width="40" height="40" xml={svg} />
-        <FontAwesomeIcon size={20} color="white" icon={faRightFromBracket} />
-      </View>
-      <View style={styles.inputField}>
-        <Text style={styles.input}>Set a Time: </Text>
-        <TextInput
-          style={styles.inputForm}
-          onChangeText={text => setTime(text)}
-          underlineColorAndroid={'white'}></TextInput>
-      </View>
-      <Text style={styles.heading}>Select a location:</Text>
-      <Pressable onPress={setGym}>
-        <View style={isGym ? styles.optionDark : styles.option}>
-          <View style={styles.details}>
+      <KeyboardAwareScrollView>
+        <View style={styles.navbar}>
+          <SvgXml width="40" height="40" xml={svg} />
+          <Pressable onPress={handleSignOut}>
             <FontAwesomeIcon
               size={20}
-              color={isGym ? '#3c2a21' : 'white'}
-              icon={faDumbbell}
+              color="white"
+              icon={faRightFromBracket}
             />
-            <Text style={isGym ? styles.optionTextDark : styles.optionText}>
-              Gym
+          </Pressable>
+        </View>
+        <Text style={styles.heading}>Select a location:</Text>
+        <Pressable onPress={setGym}>
+          <View style={location == 'Gym' ? styles.optionDark : styles.option}>
+            <View style={styles.details}>
+              <FontAwesomeIcon
+                size={20}
+                color={location == 'Gym' ? '#3c2a21' : 'white'}
+                icon={faDumbbell}
+              />
+              <Text
+                style={
+                  location == 'Gym' ? styles.optionTextDark : styles.optionText
+                }>
+                Gym
+              </Text>
+            </View>
+          </View>
+        </Pressable>
+        <Pressable onPress={setSendhoor}>
+          <View
+            style={location == 'Sendhoor' ? styles.optionDark : styles.option}>
+            <View style={styles.details}>
+              <FontAwesomeIcon
+                size={20}
+                color={location == 'Sendhoor' ? '#3c2a21' : 'white'}
+                icon={faMugSaucer}
+              />
+              <Text
+                style={
+                  location == 'Sendhoor'
+                    ? styles.optionTextDark
+                    : styles.optionText
+                }>
+                Sendhoor
+              </Text>
+            </View>
+          </View>
+        </Pressable>
+        <Pressable onPress={setIfun}>
+          <View style={location == 'ifun' ? styles.optionDark : styles.option}>
+            <View style={styles.details}>
+              <FontAwesomeIcon
+                size={20}
+                color={location == 'ifun' ? '#3c2a21' : 'white'}
+                icon={faBurger}
+              />
+              <Text
+                style={
+                  location == 'ifun' ? styles.optionTextDark : styles.optionText
+                }>
+                ifun
+              </Text>
+            </View>
+          </View>
+        </Pressable>
+        <Pressable onPress={setSoumil}>
+          <View
+            style={
+              location == `Soumil's house` ? styles.optionDark : styles.option
+            }>
+            <View style={styles.details}>
+              <FontAwesomeIcon
+                size={20}
+                color={location == `Soumil's house` ? '#3c2a21' : 'white'}
+                icon={faHouse}
+              />
+              <Text
+                style={
+                  location == `Soumil's house`
+                    ? styles.optionTextDark
+                    : styles.optionText
+                }>
+                Soumil's place
+              </Text>
+            </View>
+          </View>
+        </Pressable>
+        <View style={styles.inputField}>
+          <Text style={styles.input}>Set a Time: </Text>
+          <TextInput
+            style={styles.inputForm}
+            onChangeText={text => setTime(text)}
+            underlineColorAndroid={'white'}></TextInput>
+        </View>
+        <Pressable onPress={writeNotification}>
+          <View style={styles.notification}>
+            <Text style={styles.notificationText}>
+              {isSent ? 'Sent!' : 'Send a Notification!'}
             </Text>
           </View>
-        </View>
-      </Pressable>
-      <Pressable onPress={setSendhoor}>
-        <View style={isSendhoor ? styles.optionDark : styles.option}>
-          <View style={styles.details}>
-            <FontAwesomeIcon
-              size={20}
-              color={isSendhoor ? '#3c2a21' : 'white'}
-              icon={faMugSaucer}
-            />
-            <Text
-              style={isSendhoor ? styles.optionTextDark : styles.optionText}>
-              Sendhoor
-            </Text>
-          </View>
-        </View>
-      </Pressable>
-      <Pressable onPress={setIfun}>
-        <View style={isIfun ? styles.optionDark : styles.option}>
-          <View style={styles.details}>
-            <FontAwesomeIcon
-              size={20}
-              color={isIfun ? '#3c2a21' : 'white'}
-              icon={faBurger}
-            />
-            <Text style={isIfun ? styles.optionTextDark : styles.optionText}>
-              ifun
-            </Text>
-          </View>
-        </View>
-      </Pressable>
-      <Pressable onPress={setSoumil}>
-        <View style={isSoumil ? styles.optionDark : styles.option}>
-          <View style={styles.details}>
-            <FontAwesomeIcon
-              size={20}
-              color={isSoumil ? '#3c2a21' : 'white'}
-              icon={faHouse}
-            />
-            <Text style={isSoumil ? styles.optionTextDark : styles.optionText}>
-              Soumil's place
-            </Text>
-          </View>
-        </View>
-      </Pressable>
-      <Pressable onPress={writeNotification}>
-        <View style={styles.notification}>
-          <Text style={styles.notificationText}>
-            {isSent ? 'Sent!' : 'Send a Notification!'}
-          </Text>
-        </View>
-      </Pressable>
+        </Pressable>
+      </KeyboardAwareScrollView>
     </View>
   );
 };
